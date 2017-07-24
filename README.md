ebe20b120c35862bd8e6670d0e0355d7408cf2ce
Grace-luning-fu <grace.luning.fu@gmail.com>
2017-07-24 17:39:50 -0400

    Add Java Application

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a Java application
+
+For numbers from 1-100:
+If the number is an even number, indicate it is an even number;
+If the number is not an even number, just print the number.
---
+++ src/MyNumberProject/NumberFun.java
@@ -0,0 +1,24 @@
+package MyNumberProject;
+
+public class NumberFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=1; i<=100; i++){
+			//If the number is even,print the number and say it is an even number
+			if (i%2==0){
+					System.out.println(i+" is an even number");
+			
+			}
+			
+			//If not even, just print the number
+			else {
+			System.out.println(i);
+			
+		}
+		}
+
+	}
+
+}
